f231238320a5e5acb12e84fca6c17117878431cc
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2024-07-23 20:54:08 +0100

    cpp classes created

---
+++ FPSSystem/Source/FPSSystem/FPSSystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSSystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSSYSTEM_API AFPSSystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
