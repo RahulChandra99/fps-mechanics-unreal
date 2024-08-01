11ffc908513ad8bc4e2f9948013a5fda74391264
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2024-08-01 19:28:18 +0100

    modules setup

--- SurvivalFPS/Source/SurvivalFPS/SurvivalFPS.Build.cs
+++ SurvivalFPS/Source/SurvivalFPS/SurvivalFPS.Build.cs
@@ -8,6 +8,18 @@ public class SurvivalFPS : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"HeadMountedDisplay", 
+			"EnhancedInput",
+			"Niagara",
+			"AIModule",
+			"NavigationSystem",
+			"GameplayTasks",
+			"UMG"
+		});
 	}
 }
